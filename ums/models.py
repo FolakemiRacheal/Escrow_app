4b35e17e00834ab1b2ee9b97e40d566c507fe223
atolagbefolakeracheal <atolagbefolakeracheal@gmail.com>
2026-09-25 20:08:17 +0100

    work on the structure

---
+++ ums/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    class Role(models.TextChoice):
+        Client = "client", "Client",
+        Contractor = "contractor", "Contractor",
+        Admin = "admin", "Admin"
+
+    roles = models.CharField(max_length=50, choice= Role.choices)    
